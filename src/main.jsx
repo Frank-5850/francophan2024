500f3ebf10edc862b58ed25caeb6be9b7e073eb0
franco <phanfranco@yahoo.com>
2023-11-21 17:38:42 -0800

    add:react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home.jsx";
+import Work from "./pages/Work.jsx";
+import About from "./pages/About.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/francophan2024/",
+    element: <App />,
+    children: [
+      {
+        path: "/francophan2024/",
+        element: <Home />,
+      },
+      {
+        path: "/francophan2024/About",
+        element: <About />,
+      },
+      {
+        path: "/francophan2024/work",
+        element: <Work />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
